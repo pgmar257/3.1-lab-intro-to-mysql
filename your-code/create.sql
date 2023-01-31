e0350dcd78d661c2a1998ab9776a7f9242144507
pgmar257 <pgmar.257@gmail.com>
2023-01-31 11:16:01 +0100

    probando mysql

--- your-code/create.sql
+++ your-code/create.sql
@@ -1,159 +1,222 @@
--- MySQL Workbench Forward Engineering
+CREATE DATABASE  IF NOT EXISTS `car_stand_db` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `car_stand_db`;
+-- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: car_stand_db
+-- ------------------------------------------------------
+-- Server version	8.0.32-0ubuntu0.22.04.2
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema car_stand_db
--- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `car_stand_db` ;
+--
+-- Table structure for table `cars`
+--
 
--- -----------------------------------------------------
--- Schema car_stand_db
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `car_stand_db` DEFAULT CHARACTER SET utf8 ;
-USE `car_stand_db` ;
-
--- -----------------------------------------------------
--- Table `car_stand_db`.`manufacturers`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `car_stand_db`.`manufacturers` ;
-
-CREATE TABLE IF NOT EXISTS `car_stand_db`.`manufacturers` (
-  `manufacturer_id` INT NOT NULL AUTO_INCREMENT,
-  `manufacturer_name` VARCHAR(45) NULL,
-  PRIMARY KEY (`manufacturer_id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `car_stand_db`.`models`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `car_stand_db`.`models` ;
-
-CREATE TABLE IF NOT EXISTS `car_stand_db`.`models` (
-  `model_id` INT NOT NULL AUTO_INCREMENT,
-  `model_name` VARCHAR(45) NULL,
-  `manufacturers_manufacturer_id` INT NOT NULL,
-  PRIMARY KEY (`model_id`),
-  INDEX `fk_models_manufacturers1_idx` (`manufacturers_manufacturer_id` ASC) VISIBLE,
-  CONSTRAINT `fk_models_manufacturers1`
-    FOREIGN KEY (`manufacturers_manufacturer_id`)
-    REFERENCES `car_stand_db`.`manufacturers` (`manufacturer_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `car_stand_db`.`cars`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `car_stand_db`.`cars` ;
-
-CREATE TABLE IF NOT EXISTS `car_stand_db`.`cars` (
-  `car_id` INT NOT NULL AUTO_INCREMENT,
-  `vin` VARCHAR(17) NOT NULL,
-  `color` VARCHAR(45) NULL,
-  `year` VARCHAR(45) NULL,
-  `models_model_id` INT NOT NULL,
+DROP TABLE IF EXISTS `cars`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cars` (
+  `car_id` int NOT NULL AUTO_INCREMENT,
+  `vin` varchar(17) NOT NULL,
+  `color` varchar(45) DEFAULT NULL,
+  `year` varchar(45) DEFAULT NULL,
+  `models_model_id` int NOT NULL,
   PRIMARY KEY (`car_id`),
-  UNIQUE INDEX `vin_UNIQUE` (`vin` ASC) VISIBLE,
-  INDEX `fk_cars_models_idx` (`models_model_id` ASC) VISIBLE,
-  CONSTRAINT `fk_cars_models`
-    FOREIGN KEY (`models_model_id`)
-    REFERENCES `car_stand_db`.`models` (`model_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  UNIQUE KEY `vin_UNIQUE` (`vin`),
+  KEY `fk_cars_models_idx` (`models_model_id`),
+  CONSTRAINT `fk_cars_models` FOREIGN KEY (`models_model_id`) REFERENCES `models` (`model_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `cars`
+--
 
--- -----------------------------------------------------
--- Table `car_stand_db`.`customers`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `car_stand_db`.`customers` ;
+LOCK TABLES `cars` WRITE;
+/*!40000 ALTER TABLE `cars` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cars` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS `car_stand_db`.`customers` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `customer_id` INT NOT NULL,
-  `customer_name` VARCHAR(45) NULL,
-  `phone_number` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
-  `address` VARCHAR(45) NULL,
-  `zip_code` VARCHAR(45) NULL,
-  `city` VARCHAR(45) NULL,
-  `state_province` VARCHAR(45) NULL,
-  `country` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+--
+-- Table structure for table `customers`
+--
 
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customers` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int NOT NULL,
+  `customer_name` varchar(45) DEFAULT NULL,
+  `phone_number` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `zip_code` varchar(45) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `state_province` varchar(45) DEFAULT NULL,
+  `country` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `car_stand_db`.`stores`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `car_stand_db`.`stores` ;
+--
+-- Dumping data for table `customers`
+--
 
-CREATE TABLE IF NOT EXISTS `car_stand_db`.`stores` (
-  `store_id` INT NOT NULL AUTO_INCREMENT COMMENT '\n',
-  `store_name` VARCHAR(45) NULL,
-  PRIMARY KEY (`store_id`))
-ENGINE = InnoDB;
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES (1,20001,'Abraham Lincoln','+1 305 907 7086',NULL,'120 SW 8th St','33130','Miami','Florida','United States'),(2,30001,'Napoléon Bonaparte','+33 1 79 75 40 00',NULL,'40 Rue du Colisée','75008','Paris','Île-de-France','France'),(4,10001,'Pablo Picasso','+34 636 17 63 82',NULL,'Paseo de la Chopera 14','28045','Madrid','Madrid','Spain');
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `invoices`
+--
 
--- -----------------------------------------------------
--- Table `car_stand_db`.`salespersons`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `car_stand_db`.`salespersons` ;
-
-CREATE TABLE IF NOT EXISTS `car_stand_db`.`salespersons` (
-  `salespersons_id` INT NOT NULL AUTO_INCREMENT,
-  `staff_id` VARCHAR(45) NOT NULL,
-  `salespersons_name` VARCHAR(45) NULL,
-  `stores_store_id` INT NOT NULL,
-  PRIMARY KEY (`salespersons_id`),
-  INDEX `fk_salespersons_stores1_idx` (`stores_store_id` ASC) VISIBLE,
-  CONSTRAINT `fk_salespersons_stores1`
-    FOREIGN KEY (`stores_store_id`)
-    REFERENCES `car_stand_db`.`stores` (`store_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `car_stand_db`.`invoices`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `car_stand_db`.`invoices` ;
-
-CREATE TABLE IF NOT EXISTS `car_stand_db`.`invoices` (
-  `invoice_id` INT NOT NULL AUTO_INCREMENT,
-  `number` VARCHAR(45) NOT NULL,
-  `date` VARCHAR(45) NOT NULL,
-  `cars_car_id` INT NOT NULL,
-  `salespersons_salespersons_id` INT NOT NULL,
-  `customers_id` INT NOT NULL,
+DROP TABLE IF EXISTS `invoices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `invoices` (
+  `invoice_id` int NOT NULL AUTO_INCREMENT,
+  `number` varchar(45) NOT NULL,
+  `date` varchar(45) NOT NULL,
+  `cars_car_id` int NOT NULL,
+  `salespersons_salespersons_id` int NOT NULL,
+  `customers_id` int NOT NULL,
   PRIMARY KEY (`invoice_id`),
-  INDEX `fk_invoices_cars1_idx` (`cars_car_id` ASC) VISIBLE,
-  INDEX `fk_invoices_salespersons1_idx` (`salespersons_salespersons_id` ASC) VISIBLE,
-  INDEX `fk_invoices_customers1_idx` (`customers_id` ASC) VISIBLE,
-  CONSTRAINT `fk_invoices_cars1`
-    FOREIGN KEY (`cars_car_id`)
-    REFERENCES `car_stand_db`.`cars` (`car_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_invoices_salespersons1`
-    FOREIGN KEY (`salespersons_salespersons_id`)
-    REFERENCES `car_stand_db`.`salespersons` (`salespersons_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_invoices_customers1`
-    FOREIGN KEY (`customers_id`)
-    REFERENCES `car_stand_db`.`customers` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_invoices_cars1_idx` (`cars_car_id`),
+  KEY `fk_invoices_salespersons1_idx` (`salespersons_salespersons_id`),
+  KEY `fk_invoices_customers1_idx` (`customers_id`),
+  CONSTRAINT `fk_invoices_cars1` FOREIGN KEY (`cars_car_id`) REFERENCES `cars` (`car_id`),
+  CONSTRAINT `fk_invoices_customers1` FOREIGN KEY (`customers_id`) REFERENCES `customers` (`id`),
+  CONSTRAINT `fk_invoices_salespersons1` FOREIGN KEY (`salespersons_salespersons_id`) REFERENCES `salespersons` (`salespersons_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `invoices`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+LOCK TABLES `invoices` WRITE;
+/*!40000 ALTER TABLE `invoices` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invoices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `manufacturers`
+--
+
+DROP TABLE IF EXISTS `manufacturers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `manufacturers` (
+  `manufacturer_id` int NOT NULL AUTO_INCREMENT,
+  `manufacturer_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`manufacturer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manufacturers`
+--
+
+LOCK TABLES `manufacturers` WRITE;
+/*!40000 ALTER TABLE `manufacturers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `manufacturers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `models`
+--
+
+DROP TABLE IF EXISTS `models`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `models` (
+  `model_id` int NOT NULL AUTO_INCREMENT,
+  `model_name` varchar(45) DEFAULT NULL,
+  `manufacturers_manufacturer_id` int NOT NULL,
+  PRIMARY KEY (`model_id`),
+  KEY `fk_models_manufacturers1_idx` (`manufacturers_manufacturer_id`),
+  CONSTRAINT `fk_models_manufacturers1` FOREIGN KEY (`manufacturers_manufacturer_id`) REFERENCES `manufacturers` (`manufacturer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `models`
+--
+
+LOCK TABLES `models` WRITE;
+/*!40000 ALTER TABLE `models` DISABLE KEYS */;
+/*!40000 ALTER TABLE `models` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `salespersons`
+--
+
+DROP TABLE IF EXISTS `salespersons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `salespersons` (
+  `salespersons_id` int NOT NULL AUTO_INCREMENT,
+  `staff_id` varchar(45) NOT NULL,
+  `salespersons_name` varchar(45) DEFAULT NULL,
+  `stores_store_id` int NOT NULL,
+  PRIMARY KEY (`salespersons_id`),
+  KEY `fk_salespersons_stores1_idx` (`stores_store_id`),
+  CONSTRAINT `fk_salespersons_stores1` FOREIGN KEY (`stores_store_id`) REFERENCES `stores` (`store_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `salespersons`
+--
+
+LOCK TABLES `salespersons` WRITE;
+/*!40000 ALTER TABLE `salespersons` DISABLE KEYS */;
+/*!40000 ALTER TABLE `salespersons` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stores`
+--
+
+DROP TABLE IF EXISTS `stores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stores` (
+  `store_id` int NOT NULL AUTO_INCREMENT COMMENT '\n',
+  `store_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`store_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stores`
+--
+
+LOCK TABLES `stores` WRITE;
+/*!40000 ALTER TABLE `stores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-01-31 11:13:12
